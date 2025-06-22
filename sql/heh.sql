94f79e9829286802f4e36035ab7e6edd865cce33
akaashi <marcusinigo1205@gmail.com>
2025-06-22 19:11:32 +0800

    aa

---
+++ sql/heh.sql
@@ -0,0 +1,10 @@
+create database hh;
+
+use hh;
+
+create table applicants(
+applicantID INT PRIMARY KEY AUTO_INCREMENT,
+name varchar(50),
+email varchar(50),
+department varchar (50),
+phonenumber varchar (20));
